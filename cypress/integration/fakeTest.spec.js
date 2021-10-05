107bcae12563071090668147d479364c94bcec82
elraffy <37414789+elraffy@users.noreply.github.com>
2021-10-04 22:17:45 -0400

    API  testing with cypress

---
+++ cypress/integration/fakeTest.spec.js
@@ -0,0 +1,24 @@
+describe('API testing', () => {
+
+
+    it('first one', () => {
+
+        Cy.visit('https://rahulshettyacademy.com/angularAppdemo')
+
+        cy.intercept({
+            method : 'GET',
+            url : 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+        },
+        {
+            statusCode : 200,
+            body : [{
+                    aisle: "1947",
+                    book_name: "El principito",
+                    isbn: "ESP2021"
+                }]
+        }).as('bookretrievals')
+        cy.get("button[class='btn btn-primary']").click()
+        cy.wait('@bookretrievals')
+
+    })
+})
