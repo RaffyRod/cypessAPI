ed6472e469e5525e0831c4d084cab0ad85503f8c
elraffy <37414789+elraffy@users.noreply.github.com>
2021-10-05 21:54:38 -0400

    first one done

--- cypress/integration/fakeTest.spec.js
+++ cypress/integration/fakeTest.spec.js
@@ -16,9 +16,18 @@ describe('API testing', () => {
                     book_name: "El principito",
                     isbn: "ESP2021"
                 }]
+
         }).as('bookretrievals')
         cy.get("button[class='btn btn-primary']").click()
-        cy.wait('@bookretrievals')
+        cy.wait('@bookretrievals').should(({request, response})=>{
+
+            cy.get('tr').should('have.length' , response.body.length+1)
+            
+        })
+        cy.get('p').should('have.text', 'Oops only 1 Book available')
+
+        // length  of the respond table array
+
 
     })
 })
